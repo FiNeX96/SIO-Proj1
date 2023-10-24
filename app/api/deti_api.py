ec1d9d5ae38480873cd987807c22647e9aca6463
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-10-25 00:25:30 +0100

    dockers for both apps

--- app/api/deti_api.py
+++ app/api/deti_api.py
@@ -382,4 +382,4 @@ if __name__ == "__main__":
 
     conn.commit()
     conn.close()
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host='0.0.0.0', port=5000)
---
+++ app/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 api/deti_api.py &
+python3 website.py
